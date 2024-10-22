ba8a5a0b110217e2a8577b2edb5042ac279833ce
Dani-GT <dani_spain87@hotmail.com>
2024-10-22 19:51:56 +0200

    .

--- ejercicio1/main.js
+++ ejercicio1/main.js
@@ -2,10 +2,18 @@ const getCharacters = async () => {
     try {
     const data = await fetch("https://thronesapi.com/api/v2/Characters");
     const dataJson = await data.json();
-    console.log(dataJson)
-} catch (error) {
+    printCharacters(dataJson)
+    } catch (error) {
     console.log(error.message);
-}
+    }
+};
+
+const printCharacters = (characters) => {
+    for (const character of characters) {
+        
+        
+        
+    }
 };
 
 getCharacters();
